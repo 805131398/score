ff18dbaf83aa34647003bbbf726d89897591f28a
zhanghao <805131398@qq.com>
2020-08-21 18:30:53 +0800

    first ommit

---
+++ src/main/java/com/yuezhix/score/ScoreApplication.java
@@ -0,0 +1,26 @@
+package com.yuezhix.score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class ScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreApplication.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+
+}
